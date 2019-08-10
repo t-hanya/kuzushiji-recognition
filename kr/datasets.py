cd397c99d578c0c81892a8d807d013799ea43440
t-hanya <toshi.hanya@gmail.com>
2019-08-10 12:05:47 +0900

    Add CharCropDataset and UnicodeMapping to datasets module

--- kr/datasets.py
+++ kr/datasets.py
@@ -3,7 +3,9 @@ Kuzushiji dataset
 """
 
 
+import json
 from pathlib import Path
+import random
 
 from chainer.dataset import DatasetMixin
 import numpy as np
@@ -13,15 +15,16 @@ from PIL import Image
 
 _prj_root = Path(__file__).resolve().parent.parent
 _dataset_dir = _prj_root / 'data' / 'kuzushiji-recognition'
+_char_crop_dir = _prj_root / 'data' / 'kuzushiji-recognition-char-crop'
 
 
 class KuzushijiRecognitionDataset(DatasetMixin):
     """Kaggle Kuzushiji Recognition training dataset."""
 
     def __init__(self) -> None:
-        assert (_dataset_dir.exists(),
+        assert _dataset_dir.exists(), \
                 ('Download Kaggle Kuzushiji Recognition dataset '
-                 'and move files to <prj>/data/kuzushiji-recognition/'))
+                 'and move files to <prj>/data/kuzushiji-recognition/')
 
         self.table = pd.read_csv(_dataset_dir / 'train.csv')
         self.image_dir = _dataset_dir / 'train_images'
@@ -48,3 +51,68 @@ class KuzushijiRecognitionDataset(DatasetMixin):
             bboxes = np.empty((0, 4), dtype=np.int)
 
         return {'image': image, 'bboxes': bboxes, 'unicodes': unicodes}
+
+
+class KuzushijiUnicodeMapping:
+    """Unicode translation data."""
+
+    def __init__(self) -> None:
+        csv_path = _dataset_dir / 'unicode_translation.csv'
+
+        self._unicode_to_char = {}
+        self._index_to_unicode = {}
+        self._unicode_to_index = {}
+
+        with csv_path.open() as f:
+            lines = f.readlines()
+
+        for i, line in enumerate(lines[1:]):
+            uni, char = line.strip().split(',')
+            self._unicode_to_char[uni] = char
+            self._index_to_unicode[i] = uni
+            self._unicode_to_index[uni] = i
+
+    def __len__(self) -> int:
+        return len(self._unicode_to_char)
+
+    def unicode_to_char(self, unicode: str) -> str:
+        return self._unicode_to_char[unicode]
+
+    def index_to_unicode(self, index: int) -> str:
+        return self._index_to_unicode[index]
+
+    def unicode_to_index(self, unicode: str) -> int:
+        return self._unicode_to_index[unicode]
+
+
+class KuzushijiCharCropDataset(DatasetMixin):
+    """Kuzushiji cropped character image dataset."""
+
+    def __init__(self) -> None:
+        self.dir_path = _char_crop_dir
+        annt = json.load((_char_crop_dir / 'annotations.json').open())
+        self.data = annt['annotations']
+        self.mapping = KuzushijiUnicodeMapping()
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def get_example(self, i) -> dict:
+        data = self.data[i]
+        data = data.copy()
+        data['image'] = Image.open(self.dir_path / data['image_path'])
+        data['label'] = self.mapping.unicode_to_index(data['unicode'])
+        return data
+
+
+class RandomSampler(DatasetMixin):
+
+    def __init__(self, dataset, virtual_size=10000):
+        self.dataset = dataset
+        self.virtual_size = virtual_size
+
+    def __len__(self):
+        return self.virtual_size
+
+    def get_example(self, i):
+        return random.choice(self.dataset)
--- tests/kr_tests/test_datasets.py
+++ tests/kr_tests/test_datasets.py
@@ -7,6 +7,8 @@ import numpy as np
 from PIL import Image
 
 from kr.datasets import KuzushijiRecognitionDataset
+from kr.datasets import KuzushijiUnicodeMapping
+from kr.datasets import KuzushijiCharCropDataset
 
 
 class TestKuzushijiRecognitionDataset:
@@ -21,3 +23,25 @@ class TestKuzushijiRecognitionDataset:
         assert type(data['bboxes']) == np.ndarray
         assert type(data['unicodes']) == list
         assert type(data['unicodes'][0]) == str
+
+
+class TestKuzushijiUnicodeMapping:
+
+    def test(self):
+        mapping = KuzushijiUnicodeMapping()
+
+        assert mapping.unicode_to_char('U+0031') == '1'
+
+        assert mapping.index_to_unicode(1) == 'U+0032'
+        assert mapping.unicode_to_index('U+0032') == 1
+
+
+class TestKuzushijiCharCropDataset:
+
+    def test(self):
+        dataset = KuzushijiCharCropDataset()
+
+        data = dataset[0]
+        assert isinstance(data['image'], Image.Image)
+        assert type(data['label']) == int
+        assert len(dataset) == 683464

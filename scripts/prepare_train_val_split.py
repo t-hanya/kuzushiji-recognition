8782b5267adff12c79f66c3a8d684dfd51eb8a3c
t-hanya <toshi.hanya@gmail.com>
2019-09-13 12:23:01 +0900

    Change train-val splitting method to group split with book title

--- scripts/prepare_train_val_split.py
+++ scripts/prepare_train_val_split.py
@@ -1,5 +1,9 @@
 """
 Prepare train / validation split.
+
+The first block of image file name separated by '-' or '_' is defined as
+book title here, and group split with book title is used to provide
+cross validation set.
 """
 
 
@@ -7,30 +11,40 @@ from pathlib import Path
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import GroupKFold
 
 
 PRJ_ROOT = Path(__file__).resolve().parent.parent
-TRAIN_CSV = PRJ_ROOT / 'data' / 'kuzushiji-recognition' / 'train.csv'
-OUTPUT_ROOT = PRJ_ROOT / 'data' / 'kuzushiji-recognition-converted'
+TRAINVAL_CSV = PRJ_ROOT / 'data' / 'kuzushiji-recognition' / 'train.csv'
+TRAINVAL_IMAGE_DIR = PRJ_ROOT / 'data' / 'kuzushiji-recognition' / 'train_images'
+OUTPUT_ROOT = PRJ_ROOT / 'data' / 'kuzushiji-recognition-gsplit'
 
-TRAIN_RATIO = 0.95
+N_SPLIT = 4
+
+
+def extract_book_title(image_path: Path) -> str:
+    """Extract book title from image file path."""
+    return image_path.parts[-1].split('_')[0].split('-')[0]
 
 
 if __name__ == '__main__':
 
     OUTPUT_ROOT.mkdir(exist_ok=True)
 
-    table = pd.read_csv(str(TRAIN_CSV))
-    n_samples = len(table)
-    n_train = int(n_samples * TRAIN_RATIO)
+    image_paths = sorted(TRAINVAL_IMAGE_DIR.iterdir())
+    book_titles = sorted(set([extract_book_title(p) for p in image_paths]))
+    group_labels = group_labels = [book_titles.index(extract_book_title(p))
+                                   for p in image_paths]
+
+    table = pd.read_csv(str(TRAINVAL_CSV))
 
     np.random.seed(0)
-    random_order = np.random.permutation(n_samples)
-    train_indices = random_order[:n_train]
-    val_indices = random_order[n_train:]
+    cv = GroupKFold(n_splits=4)
+    splits = cv.split(image_paths, groups=group_labels)
 
-    table_train = table.iloc[train_indices]
-    table_val = table.iloc[val_indices]
+    for i, (train_indices, val_indices) in enumerate(splits):
+        table_train = table.iloc[train_indices]
+        table_val = table.iloc[val_indices]
 
-    table_train.to_csv(str(OUTPUT_ROOT / 'train.csv'), index=False)
-    table_val.to_csv(str(OUTPUT_ROOT / 'val.csv'), index=False)
+        table_train.to_csv(str(OUTPUT_ROOT / f'train-{i}.csv'), index=False)
+        table_val.to_csv(str(OUTPUT_ROOT / f'val-{i}.csv'), index=False)

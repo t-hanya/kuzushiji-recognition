05772f572d82c8e4b219cd8087f4cd5789ce4a5a
t-hanya <toshi.hanya@gmail.com>
2019-09-20 08:06:44 +0900

    Add OverSampler

--- kr/datasets/__init__.py
+++ kr/datasets/__init__.py
@@ -3,3 +3,4 @@ from kr.datasets.kuzushiji_recognition import KuzushijiUnicodeMapping
 from kr.datasets.kuzushiji_recognition import KuzushijiCharCropDataset
 from kr.datasets.kuzushiji_recognition import KuzushijiTestImages
 from kr.datasets.sampling import RandomSampler
+from kr.datasets.sampling import OverSampler
--- tests/kr_tests/datasets_tests/test_sampling.py
+++ tests/kr_tests/datasets_tests/test_sampling.py
@@ -3,7 +3,11 @@ Unittest for kr.datasets.sampling module.
 """
 
 
+from chainer.dataset import DatasetMixin
+import numpy as np
+
 from kr.datasets import RandomSampler
+from kr.datasets import OverSampler
 
 
 class TestRandomSampler:
@@ -13,3 +17,25 @@ class TestRandomSampler:
         assert len(dataset) == 10
         for _ in range(10):
             assert 1 <= dataset[0] <= 5
+
+
+class DummyDataset(DatasetMixin):
+
+    def __init__(self):
+        self.all_labels = np.array([0, 0, 0, 0, 0, 1])
+
+    def __len__(self):
+        return len(self.all_labels)
+
+    def get_example(self, i):
+        return self.all_labels[i]
+
+
+class TestOverSampler:
+
+    def test(self):
+        dataset = OverSampler(DummyDataset(), min_samples=5)
+        expected = np.array([0.5, 0.5], dtype=np.float32)
+        assert np.isclose(dataset.class_probs, expected).all()
+        for _ in range(10):
+            assert dataset[0] in (0, 1)

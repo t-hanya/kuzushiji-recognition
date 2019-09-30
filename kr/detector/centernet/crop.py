7a0748793bcaf97abb7990f194f51bba0d9aa12e
t-hanya <toshi.hanya@gmail.com>
2019-09-30 22:15:25 +0900

    Modify crop function to keep aspect ratio

--- kr/detector/centernet/crop.py
+++ kr/detector/centernet/crop.py
@@ -29,12 +29,11 @@ class RandomCropAndResize:
 
         org_w, org_h = image.size
 
-        # crop
-        crop_w = int(np.random.uniform(*self.scale_range) * org_w)
-        crop_h = int(np.random.uniform(*self.scale_range) * org_h)
-        crop_x = np.random.randint(0, org_w - crop_w + 1)
-        crop_y = np.random.randint(0, org_h - crop_h + 1)
-        cropped = image.crop((crop_x, crop_y, crop_x + crop_w, crop_y + crop_h))
+        crop_size = int(np.random.uniform(*self.scale_range) * min(org_w, org_h))
+        crop_x = np.random.randint(0, org_w - crop_size + 1)
+        crop_y = np.random.randint(0, org_h - crop_size + 1)
+        cropped = image.crop((crop_x, crop_y,
+                              crop_x + crop_size, crop_y + crop_size))
 
         bboxes = bboxes.copy()
         bboxes[:, 0::2] -= crop_x
@@ -44,16 +43,16 @@ class RandomCropAndResize:
         mask = np.logical_and.reduce((
             0 < centers[:, 0],
             0 < centers[:, 1],
-            centers[:, 0] < crop_w,
-            centers[:, 1] < crop_h
+            centers[:, 0] < crop_size,
+            centers[:, 1] < crop_size
         ))
         bboxes = bboxes[mask]
         unicodes = [u for u, m in zip(unicodes, mask) if m]
 
         # resize
         resized = cropped.resize(self.size, resample=Image.BILINEAR)
-        bboxes[:, 0::2] = bboxes[:, 0::2] * (self.size[0] / crop_w)
-        bboxes[:, 1::2] = bboxes[:, 1::2] * (self.size[1] / crop_h)
+        bboxes[:, 0::2] = bboxes[:, 0::2] * (self.size[0] / crop_size)
+        bboxes[:, 1::2] = bboxes[:, 1::2] * (self.size[1] / crop_size)
 
         return resized, bboxes, unicodes
 
@@ -78,11 +77,11 @@ class CenterCropAndResize:
         org_w, org_h = image.size
 
         # crop
-        crop_w = int(self.scale * org_w)
-        crop_h = int(self.scale * org_h)
-        crop_x = (org_w - crop_w) // 2
-        crop_y = (org_h - crop_h) // 2
-        cropped = image.crop((crop_x, crop_y, crop_x + crop_w, crop_y + crop_h))
+        crop_size = int(self.scale * min(org_w, org_h))
+        crop_x = (org_w - crop_size) // 2
+        crop_y = (org_h - crop_size) // 2
+        cropped = image.crop((crop_x, crop_y,
+                              crop_x + crop_size, crop_y + crop_size))
 
         bboxes = bboxes.copy()
         bboxes[:, 0::2] -= crop_x
@@ -92,15 +91,15 @@ class CenterCropAndResize:
         mask = np.logical_and.reduce((
             0 < centers[:, 0],
             0 < centers[:, 1],
-            centers[:, 0] < crop_w,
-            centers[:, 1] < crop_h
+            centers[:, 0] < crop_size,
+            centers[:, 1] < crop_size
         ))
         bboxes = bboxes[mask]
         unicodes = [u for u, m in zip(unicodes, mask) if m]
 
         # resize
         resized = cropped.resize(self.size, resample=Image.BILINEAR)
-        bboxes[:, 0::2] = bboxes[:, 0::2] * (self.size[0] / crop_w)
-        bboxes[:, 1::2] = bboxes[:, 1::2] * (self.size[1] / crop_h)
+        bboxes[:, 0::2] = bboxes[:, 0::2] * (self.size[0] / crop_size)
+        bboxes[:, 1::2] = bboxes[:, 1::2] * (self.size[1] / crop_size)
 
         return resized, bboxes, unicodes
